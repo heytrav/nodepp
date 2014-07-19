30295bf74379ec33ddfe6dd5bb512e2aef031b2f
Travis Holton <travis@catalyst.net.nz>
2014-07-20 11:36:34 +1200

    refactor command as a dispatcher

--- test/test.js
+++ test/test.js
@@ -15,33 +15,37 @@ describe('EPP state machine', function() {
 	});
 	it('should start out offline', function() {
 		expect(stateMachine.state).to.equal('offline');
-        expect(stateMachine.connected).to.equal(false);
+		expect(stateMachine.connected).to.equal(false);
 	});
 
-    describe('EPP Logged in', function() {
-    
-        beforeEach(function(){
-            stateMachine.login({
-                "login": "test-user",
-                "password": "123xyz"
-            },
-            function() {
-                return {
-                    "status": "OK"
-                };
-            });
-        });
-        it('should move into idle loop following successful login', function() {
-            expect(stateMachine.connected).to.equal(true);
-            expect(stateMachine.state).to.equal('idle');
+	describe('EPP Logged in', function() {
 
-        });
-        it('should be disconnected after logging out', function() {
-            stateMachine.logout(function() {
-                return {"status": "OK"};
-            });
-            expect(stateMachine.connected).to.equal(false);
-        });
-    });
+		beforeEach(function() {
+			stateMachine.command('login', {
+				"login": "test-user",
+				"password": "123xyz"
+			},
+			function() {
+				return {
+					"status": "OK"
+				};
+			});
+		});
+		it('should move into idle loop following successful login', function() {
+			expect(stateMachine.connected).to.equal(true);
+			expect(stateMachine.state).to.equal('idle');
+
+		});
+		it('should be disconnected after logging out', function() {
+			stateMachine.command('logout', {},
+
+			function() {
+				return {
+					"status": "OK"
+				};
+			});
+			expect(stateMachine.connected).to.equal(false);
+		});
+	});
 });
 
