09ff439f58b877f700ae9a53321821496bb56dd8
Travis Holton <travis@catalyst.net.nz>
2014-07-20 15:41:26 +1200

    be a little more descriptive in test

--- test/epp-generate.js
+++ test/epp-generate.js
@@ -7,11 +7,10 @@ var EPP = require('../lib/epp.js');
 
 describe('EPP serialisation', function () {
     var epp;
-    it('should be an epp object', function() {
+    it('should be an epp object with nzrs config', function() {
         epp = new EPP('nzrs');
         expect(epp).to.be.an.instanceof(Object);
         var config = epp.config;
         expect(config.namespaces.epp.ns).to.be.equal('urn:ietf:params:xml:ns:epp-1.0');
-        
     });
 });
