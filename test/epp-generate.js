c850f8f20fb4e2fec2805cefdad5f5e1a94cae0a
Travis Holton <travis@catalyst.net.nz>
2014-07-20 16:00:23 +1200

    return xml data from wrapper

--- test/epp-generate.js
+++ test/epp-generate.js
@@ -7,8 +7,11 @@ var EPP = require('../lib/epp.js');
 
 describe('EPP serialisation', function () {
     var epp;
-    it('should be an epp object with nzrs config', function() {
+    beforeEach(function () {
         epp = new EPP('nzrs');
+    });
+
+    it('should be an epp object with nzrs config', function() {
         expect(epp).to.be.an.instanceof(Object);
         var config = epp.config;
         expect(config.namespaces.epp.ns).to.be.equal('urn:ietf:params:xml:ns:epp-1.0');
