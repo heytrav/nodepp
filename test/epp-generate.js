7b49e09d7c3ca44cf6219ba0937e12d422e0d585
Travis Holton <travis@catalyst.net.nz>
2014-07-20 15:40:43 +1200

    setup to read from config file

--- test/epp-generate.js
+++ test/epp-generate.js
@@ -8,9 +8,10 @@ var EPP = require('../lib/epp.js');
 describe('EPP serialisation', function () {
     var epp;
     it('should be an epp object', function() {
-        epp = new EPP();
+        epp = new EPP('nzrs');
         expect(epp).to.be.an.instanceof(Object);
-
+        var config = epp.config;
+        expect(config.namespaces.epp.ns).to.be.equal('urn:ietf:params:xml:ns:epp-1.0');
         
     });
 });
