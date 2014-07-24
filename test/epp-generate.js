b53338e8ebb4b6f8f7d898347da122031eade206
Travis Holton <travis@catalyst.net.nz>
2014-07-25 08:41:23 +1200

    add test for generating "checkDomain"

--- test/epp-generate.js
+++ test/epp-generate.js
@@ -1,37 +1,53 @@
 var chai = require('chai');
 
 var expect = chai.expect,
-    should = chai.should;
+should = chai.should;
 
 var EPP = require('../lib/epp.js');
-var config = require('../lib/epp-config.json').nzrs;
+var mainConfig = require('../lib/epp-config.json');
 
-describe('EPP serialisation', function () {
+describe('EPP serialisation', function() {
     var epp;
-    beforeEach(function () {
-        epp = new EPP('nzrs', config);
+    describe('NZRS specific', function() {
+        var config;
+        beforeEach(function() {
+            config = mainConfig.nzrs;
+            epp = new EPP('nzrs', config);
+        });
+
+        it('should be an epp object with nzrs config', function() {
+            expect(epp).to.be.an.instanceof(Object);
+            expect(config.namespaces.epp.xmlns).to.be.equal('urn:ietf:params:xml:ns:epp-1.0');
+        });
+
+        it('should generate an xml body', function() {
+            var xml = epp.login({
+                "login": "user1",
+                "password": "abc123"
+            },
+            'test-1234');
+            console.log("Got xml: ", xml);
+            expect(xml).to.match(/<login>/);
+        });
+
+        it('should generate a hello command', function() {
+            var xml = epp.hello();
+            console.log("Got hello: ", xml);
+            expect(xml).to.match(/<hello\/>/);
+        });
+
+        it('should generate a logout command', function() {
+            var xml = epp.logout('test-1235');
+            console.log("Got logout: ", xml);
+            expect(xml).to.match(/<logout\/>/);
+        });
+
+        it('should generate a checkDomain command', function() {
+            var xml = epp.checkDomain( {"domain": "test-domain.com"}, 'test-12345');
+            expect(xml).to.match(/<check>(?:(?!<\/check>).)*<\/check>/);
+        });
+
     });
 
-    it('should be an epp object with nzrs config', function() {
-        expect(epp).to.be.an.instanceof(Object);
-        expect(config.namespaces.epp.xmlns).to.be.equal('urn:ietf:params:xml:ns:epp-1.0');
-    });
-
-    it('should generate an xml body', function() {
-        var xml = epp.login({"login": "user1", "password": "abc123"}, 'test-1234');
-        console.log("Got xml: ", xml);
-        expect(xml).to.match(/<login>/);
-    });
-
-    it('should generate a hello command', function() {
-        var xml = epp.hello();
-        console.log("Got hello: ", xml);
-        expect(xml).to.match(/<hello\/>/);
-    });
-
-    it('should generate a logout command', function() {
-        var xml = epp.logout('test-1235');
-        console.log("Got logout: ", xml);
-        expect(xml).to.match(/<logout\/>/);
-    });
 });
+
