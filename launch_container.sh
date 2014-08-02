1c1c5e8f136b68f371e1c62d7129b7690ed2d0a9
Travis Holton <travis@catalyst.net.nz>
2014-08-03 09:43:35 +1200

    add options to launch

---
+++ launch_container.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+set -e
+
+while getopts ":r:e:p:h"; do
+  case "${opt}" in
+    r)
+      REGISTRY+=("$OPTARG")
+      ;;
+    e)
+      ENVIRONMENT=${OPTARG}
+      ;;
+    p)
+      PORT=${OPTARG}
+      ;;
+  esac
+done
+shift $(($OPTIND-1))
+
+# set some defaults
+${ENVIRONMENT:=devel}
+${PORT:=3000}
+
+gpg -d A000A000000000000052.pem.asc > A000A000000000000052.pem
+gpg -d iwantmyname.com.key.org.asc > iwantmyname.com.key
+
+docker run -v /usr/local/d8o/nodepp:/usr/local/d8o/nodepp -p ${PORT}:${PORT} -e ENVIRONMENT=$ENVIRONMENT -v /usr/local/d8o/etc/ssl/certs:/usr/local/d8o/etc/ssl/certs -t -i -d docker.domarino.com/nodepp ./docker_start.sh
