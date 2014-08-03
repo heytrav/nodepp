2f263a8d0d8324a39fa524ed61f211d7bdb5c492
Travis Holton <travis@catalyst.net.nz>
2014-08-03 12:39:42 +1200

    choose between running shell or actual script

--- launch_container.sh
+++ launch_container.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-while getopts ":r:e:p:h"; do
+while getopts ":r:se:p:h" opt; do
   case "${opt}" in
+    s)
+        ACTION="shell"
+        ;;
     r)
       REGISTRY+=("$OPTARG")
       ;;
@@ -18,15 +21,19 @@ done
 shift $(($OPTIND-1))
 
 # set some defaults
-${ENVIRONMENT:=devel}
-${PORT:=3000}
-${REGISTRY:=hexonet-test1}
+echo $REGISTRY
+: ${ENVIRONMENT:=devel}
+: ${PORT:=3000}
+: ${REGISTRY:=hexonet-test1}
+: ${ACTION:=run}
+
+case "${ACTION}" in
+    shell )
+        echo "docker run -t -i -v /usr/local/d8o/nodepp:/usr/local/d8o/nodepp  -v /usr/local/d8o/etc/ssl/certs:/usr/local/d8o/etc/ssl/certs docker.domarino.com/nodepp /bin/bash"
+        ;;
+    run ) 
+        echo "docker run -t -i -d -v /usr/local/d8o/nodepp:/usr/local/d8o/nodepp -p ${PORT}:${PORT} -e EPP_ENVIRONMENT=${ENVIRONMENT} -e EPP_REGISTRIES=${REGISTRY} -v /usr/local/d8o/etc/ssl/certs:/usr/local/d8o/etc/ssl/certs docker.domarino.com/nodepp ./docker_start.sh"
+        ;;
+esac
 
 
-docker run -t -i -d \
-  -v /usr/local/d8o/nodepp:/usr/local/d8o/nodepp \
-  -p ${PORT}:${PORT} \
-  -e EPP_ENVIRONMENT=$ENVIRONMENT \
-  -e EPP_REGISTRIES=$REGISTRY \
-  -v /usr/local/d8o/etc/ssl/certs:/usr/local/d8o/etc/ssl/certs \
-  docker.domarino.com/nodepp ./docker_start.sh
