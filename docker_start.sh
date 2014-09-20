e067da6b267115149b6ba55ce6a0aa0c57db6afa
Travis Holton <travis@catalyst.net.nz>
2014-09-21 08:37:19 +1200

    adding docker setup

--- docker_start.sh
+++ docker_start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo "Creating cert file"
-#gpg -d A000A000000000000052.pem.asc > /usr/local/d8o/etc/ssl/certs/A000A000000000000052.pem
-#gpg -d iwantmyname.com.key.org.asc > /usr/local/d8o/etc/ssl/certs/iwantmyname.com.key
-npm start 
+echo "Link ${EPP_ENVIRONMENT} config"
+
+CONFIG="lib/epp-config-${EPP_ENVIRONMENT}.json"
+
+if [ -e $CONFIG ]; then
+    ln -sf $CONFIG  lib/epp-config.json
+fi
+
+
+npm start
--- launch_container.sh
+++ launch_container.sh
@@ -22,4 +22,9 @@ ${ENVIRONMENT:=devel}
 ${PORT:=3000}
 
 
-docker run -v /usr/local/d8o/nodepp:/usr/local/d8o/nodepp -p ${PORT}:${PORT} -e ENVIRONMENT=$ENVIRONMENT -v /usr/local/d8o/etc/ssl/certs:/usr/local/d8o/etc/ssl/certs -t -i -d docker.domarino.com/nodepp ./docker_start.sh
+docker run -t -i -d \
+  -v /usr/local/d8o/nodepp:/usr/local/d8o/nodepp \
+  -p ${PORT}:${PORT} \
+  -e EPP_ENVIRONMENT=$ENVIRONMENT \
+  -v /usr/local/d8o/etc/ssl/certs:/usr/local/d8o/etc/ssl/certs \
+  docker.domarino.com/nodepp ./docker_start.sh
