8992cf6a5a4bc3de1a94f0d3ffa8f2e469049f46
Travis Holton <travis@ideegeo.com>
2015-09-07 06:58:28 +1200

    use supervisor to start up

--- docker_start.sh
+++ docker_start.sh
@@ -5,17 +5,3 @@ if [ "$INTERACTIVE" = 1 ]; then
 else
     /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
 fi
-
-#: ${EPP_ENVIRONMENT:=devel}
-#echo "Link ${EPP_ENVIRONMENT} config"
-
-#CONFIG="config/epp-config-${EPP_ENVIRONMENT}.json"
-
-#if [ -e $CONFIG ]; then
-    #ln -sf $CONFIG  config/epp-config.json
-#fi
-
-
-#npm start
-
-
